e3b4a55af386a3fdf81f5b6516b223384952dd95
olawanlejoel <wardgray24@gmail.com>
2023-10-22 00:13:05 +0100

    render all connected players on client

--- src/App.jsx
+++ src/App.jsx
@@ -28,15 +28,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 
+function getHeightWidth() {
+  return [window.innerHeight, window.innerWidth];
+}
+
 export default function App() {
-  function getHeightWidth() {
-    return [window.innerHeight, window.innerWidth];
-  }
   const canvasRef = useRef(null);
   const [heightWidth, setHeightWidth] = useState(getHeightWidth());
   const [playerRef, setPlayerRef] = useState(null);
   const [playerId, setPlayerId] = useState(null);
-
+  const [playerPositions, setPlayerPositions] = useState(null);
   useEffect(() => {
     const auth = getAuth();
     signInAnonymously(auth).then(() => {
@@ -45,58 +46,70 @@ export default function App() {
     onAuthStateChanged(auth, (player) => {
       console.log(player);
       setPlayerId(player.uid);
-      const reference = ref(db, "players/" + player.uid);
+      const playerRef = ref(db, "players/" + player.uid);
+      const allPlayers = ref(db, "players/");
       window.addEventListener("resize", () => setHeightWidth(getHeightWidth));
       window.addEventListener("keydown", (e) => {
         const pressedKey = e.key.toLowerCase();
         if (pressedKey === "w") {
-          runTransaction(reference, (prevData) => {
+          runTransaction(playerRef, (prevData) => {
             const newPos = prevData.pos;
             newPos[1]--;
             return { ...prevData, pos: newPos };
           });
         }
         if (pressedKey === "s") {
-          runTransaction(reference, (prevData) => {
+          runTransaction(playerRef, (prevData) => {
             const newPos = prevData.pos;
             newPos[1]++;
             return { ...prevData, pos: newPos };
           });
         }
         if (pressedKey === "a") {
-          runTransaction(reference, (prevData) => {
+          runTransaction(playerRef, (prevData) => {
             const newPos = prevData.pos;
             newPos[0]--;
             return { ...prevData, pos: newPos };
           });
         }
         if (pressedKey === "d") {
-          runTransaction(reference, (prevData) => {
+          runTransaction(playerRef, (prevData) => {
             const newPos = prevData.pos;
             newPos[0]++;
             return { ...prevData, pos: newPos };
           });
         }
       });
-      setPlayerRef(reference);
-      set(reference, {
+      setPlayerRef(playerRef);
+      set(playerRef, {
         pos: [0, 0, 50, 50],
       });
-      onValue(reference, (snapshot) => {
+      onValue(playerRef, (snapshot) => {
         const data = snapshot.val();
         setPos(data.pos);
+      });
+      onValue(allPlayers, (snapshot) => {
+        const data = snapshot.val();
+        const positions = Object.keys(data).map((playerKey) => {
+          return data[playerKey].pos;
+        });
+        setPlayerPositions(positions);
       });
     });
   }, []);
 
   const [pos, setPos] = useState(0);
   const draw = (context) => {
+    // context.clearRect(0, 0, heightWidth[0], heightWidth[1]);
+    // context.fillStyle = "rgb(200, 0, 0)";
+    // context.fillRect(pos[0], pos[1], pos[2], pos[3]);
+    // context.fillStyle = "rgba(0, 0, 200, 0.5)";
+    // context.fillRect(50, 50, 50, 50);
     context.clearRect(0, 0, heightWidth[0], heightWidth[1]);
-    context.fillStyle = "rgb(200, 0, 0)";
-    context.fillRect(pos[0], pos[1], pos[2], pos[3]);
-
-    context.fillStyle = "rgba(0, 0, 200, 0.5)";
-    context.fillRect(50, 50, 50, 50);
+    playerPositions?.forEach((pos) => {
+      context.fillStyle = "rgb(200, 0, 0)";
+      context.fillRect(pos[0], pos[1], pos[2], pos[3]);
+    });
   };
 
   if (!playerId) return <h1>Loading</h1>;
